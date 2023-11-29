98be522f2a2672d136a495693ee6f30f1db385db
Newbiecoder-09 <146342355+Newbiecoder-09@users.noreply.github.com>
2023-11-30 02:25:28 +0530

    Update snakeGame.py

--- snake game/snakeGame.py
+++ snake game/snakeGame.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-
+#file path
 score_file = "snake game/static/highscore.txt"
 
 
@@ -10,26 +10,25 @@ score_file = "snake game/static/highscore.txt"
 def home():
     return render_template('index.html')
 
-
+#sending the Highscore value
 @app.route('/GetHighScore', methods=['GET'])
 def GetHighScore():
-    # Read the high score value from 'highscore.txt'
     with open(score_file, 'r') as file:
         content = file.read()
 
     data_dict = {"data": content}
     return jsonify(data_dict)
 
-
+#receiving highscore data from game
 @app.route('/HighScore', methods=['POST'])
 def HighScore():
-    # Get the JSON data from the request
     json_data = request.get_json()
     highscore_value = json_data.get('highscore')
 
     with open(score_file, 'r') as file:
         content = file.read()
-    if content < highscore_value:
+        
+    if content < highscore_value:  #compare the previous highscore with current score
         with open(score_file, 'w') as file:
             file.write(str(highscore_value))
 
@@ -37,4 +36,4 @@ def HighScore():
 
 
 if __name__ == "__main__":
-    app.run(debug=True) //run at local host
+    app.run(debug=True) #run at local host
